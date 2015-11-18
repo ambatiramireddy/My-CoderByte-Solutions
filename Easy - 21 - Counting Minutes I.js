fe912c6f5e9f57bddf1cd2e9d284365e6a5de2e8
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 12:06:08 +0530

    Update Easy - 21 - Counting Minutes I.js

--- Easy - 21 - Counting Minutes I.js
+++ Easy - 21 - Counting Minutes I.js
@@ -1,33 +1,24 @@
-function CountingMinutesI(str) {
+function CountingMinutesI(str) { 
+  
+  function ParseTime(time) {
+    var timeParts = time.split(':');
+    var hours = parseInt(timeParts[0]);
+    var minutes = parseInt(timeParts[1].substr(0, 2));
+    var totalMinutes = hours * 60 + minutes;
+    var AMPM = timeParts[1].substr(2, 2).toUpperCase();
+    return { AMPM: AMPM, minutes: totalMinutes };
+  }
 
-    var timeArray = str.split("-");
-    var startTime = timeArray[0];
-    var endTime = timeArray[1];
-    var startTimeArr = startTime.split(":");
-    var startTimeHour = parseInt(startTimeArr[0]);
-    var startTimeMinute = parseInt(startTimeArr[1].replace(/[^0-9]/g, ""));
-    var startTimeAMPM = startTimeArr[1].replace(/[^a-zA-Z]/g, "");
-    var endTimeArr = endTime.split(":");
-    var endTimeHour = parseInt(endTimeArr[0]);
-    var endTimeMinute = parseInt(endTimeArr[1].replace(/[^0-9]/g, ""));
-    var endTimeAMPM = endTimeArr[1].replace(/[^a-zA-Z]/g, "");
+  var times = str.split('-')
+  var time1 = ParseTime(times[0]);
+  var time2 = ParseTime(times[1]);
 
-    if ((startTimeHour !== 12) && (startTimeAMPM === "pm")) {
-        startTimeHour += 12;
-    }
-    if ((endTimeHour !== 12) && (endTimeAMPM === "pm")) {
-        endTimeHour += 12;
-    }
-    if ((startTimeHour === 12) && (startTimeAMPM === "am")) {
-        startTimeHour -= 12;
-    }
-    if ((endTimeHour === 12) && (endTimeAMPM === "am")) {
-        endTimeHour -= 12;
-    }
+  if (time1.AMPM != time2.AMPM) {
+    time2.minutes += 12 * 60;
+  }
+  else if (time1.AMPM == time2.AMPM && time1.minutes > time2.minutes) {
+    time2.minutes += 24 * 60;
+  }
 
-    if ((startTimeHour > endTimeHour) || ((startTimeHour === endTimeHour) && (startTimeMinute > endTimeMinute))) {
-        return (1440 + ((endTimeHour - startTimeHour) * 60) + (endTimeMinute - startTimeMinute));
-    } else {
-        return ((endTimeHour - startTimeHour) * 60) + (endTimeMinute - startTimeMinute);
-    }
+  return time2.minutes - time1.minutes;
 }
