c9fd298b373d0126797caad8fc3cf520cd9763ef
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 14:45:19 +0530

    Update Medium - 27 - Multiple Brackets.js

--- Medium - 27 - Multiple Brackets.js
+++ Medium - 27 - Multiple Brackets.js
@@ -1,34 +1,21 @@
 function MultipleBrackets(str) {
+   var openBrackets = [], unmatchedBracketfound = false, bracketsCount = 0;
+   for (var i = 0; i < str.length; i++) {
+     var c = str[i];
+     if (c == '(' || c == '[') {
+       openBrackets.push(c);
+       bracketsCount++;
+     }
+     else if (c == ')' || c == ']') {
+       var lastOpenBracket = openBrackets.pop();
+       if ((c == ')' && lastOpenBracket != '(') ||
+           (c == ']' && lastOpenBracket != '['))
+         unmatchedBracketfound = true;
+     }
+   }
 
-    var leftPCount = 0;
-    var rightPCount = 0;
-    var leftBCount = 0;
-    var rightBCount = 0;
-
-    for (var i = 0; i <= str.length; i++) {
-        if (str[i] == "(") {
-            leftPCount++;
-        } else if (str[i] == ")") {
-            rightPCount++;
-        }
-        if (rightPCount > leftPCount) {
-            return 0;
-        }
-
-        if (str[i] == "[") {
-            leftBCount++;
-        } else if (str[i] == "]") {
-            rightBCount++;
-        }
-        if (rightBCount > leftBCount) {
-            return 0;
-        }
-
-    }
-
-    if ((leftPCount == rightPCount) && (leftBCount == rightBCount)) {
-        return "1 " + (leftPCount + leftBCount);
-    }
-    return 0;
-
-}
+   if (unmatchedBracketfound || openBrackets.length > 0)
+     return 0;
+   else
+     return 1 + ' ' + bracketsCount;
+ }
