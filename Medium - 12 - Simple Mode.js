84c486c98054b205d49a0035aeca92e8a15540d2
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 12:49:20 +0530

    Update Medium - 12 - Simple Mode.js

--- Medium - 12 - Simple Mode.js
+++ Medium - 12 - Simple Mode.js
@@ -1,29 +1,23 @@
 function SimpleMode(arr) {
+  arr.sort(function (a, b) { return a - b; });
+  var runChar = arr[0], runCount = 1;
+  var longRunChar = arr[0], longRunCount = 1;
+  for (var i = 1; i < arr.length; i++) {
+    var v = arr[i];
+    if (runChar == v)
+      runCount++;
+    else {
+      if (runCount > longRunCount) {
+        longRunCount = runCount;
+        longRunChar = runChar;
+      }
+      runChar = v;
+      runCount = 1;
+    }
+  }
 
-    var map = {};
-    var mode = 0;
-    var highestFreq = 0;
-
-    for (i = 0; i < arr.length; i++) {
-        if (map[arr[i]]) {
-            map[arr[i]]++;
-        } else {
-            map[arr[i]] = 1;
-        }
-    }
-    
-    for (i = 0; i < arr.length; i++) {
-        if (map[arr[i]] > highestFreq) {
-            highestFreq = map[arr[i]];
-            mode = arr[i];
-        }
-    }
-    
-    if (highestFreq !== 1) {
-        return mode;
-    } else {
-        return -1;
-    }
-    
-    return mode;
+  if (longRunCount > 1)
+    return longRunChar;
+  else
+    return -1;
 }
