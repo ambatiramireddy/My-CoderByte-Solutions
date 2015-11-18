693eefe9a201b039a1fcc5e1e88be71a046f8288
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 12:07:36 +0530

    Update Easy - 22 - Mean Mode.js

--- Easy - 22 - Mean Mode.js
+++ Easy - 22 - Mean Mode.js
@@ -1,80 +1,26 @@
-// Step By Step
-function MeanMode(arr) {
-    
-    // First, we declare an empty array to hold the number of times each item in the input array occurs,
-    var modeMap = [];
-    // a variable to hold the actual mode,
-    var mode = 0;
-    // a variable to hold the number of times the mode occurs,
-    var modeCount = 0;
-    // and a variable to hold the sum of the items in the array (to calculate the mean),
-    var sum = 0;
-    
-    // Next, we loop through each item in the input array and... 
-    for (var i = 0; i < arr.length; i++) {
-        
-        // ...add each value to our sum variable
-        sum += arr[i];
-        
-        // ...check if each value is present in our frequency count array
-        if (modeMap[arr[i]]) {
-            // If it is, we increase its value by 1.
-            modeMap[arr[i]]++;
-        } else {
-            // If it isn't, we set its value to 1.
-            modeMap[arr[i]] = 1;
-        }
-        
-        // ...and check if the count for the item's position in our mode array is greater than our current mode's count.
-        if (modeMap[arr[i]] > modeCount) {
-            // If it is, we set the mode to the current item in our input array...
-            mode = arr[i];
-            // ...and set the number of times the mode has occured to the current item's value in our mode array.
-            modeCount = modeMap[arr[i]];
-        }
+function MeanMode(arr) { 
+  arr.sort(function (a, b) { return a - b; });
+  var runChar = arr[0], runCount = 1;
+  var longRunChar = arr[0], longRunCount = 1;
+  for (var i = 1; i < arr.length; i++) {
+    var v = arr[i];
+    if (runChar == v)
+      runCount++;
+    else {
+      if (runCount > longRunCount) {
+        longRunChar = runChar;
+        longRunCount = runCount;
+      }
+      runChar = v;
+      runCount = 1;
     }
-    
-    // Next, we calculate the mean by dividing the sum by the total number of items in the array.
-    var mean = sum / arr.length;
-    
-    // If the mean and the mode are the same, we return 1, otherwise we return 0.
-    if (mean === mode) {
-        return 1;
-    } else {
-        return 0;
-    }
+  }
 
-}
+  var mode = longRunChar;
+  var mean = arr.reduce(function (pv, v) { return pv + v; }) / arr.length;
 
-// Without Comments
-function MeanMode(arr) {
-
-    var modeMap = [];
-    var mode = 0;
-    var modeCount = 0;
-    var sum = 0;
-
-    for (var i = 0; i < arr.length; i++) {
-
-        sum += arr[i];
-
-        if (modeMap[arr[i]]) {
-            modeMap[arr[i]]++;
-        } else {
-            modeMap[arr[i]] = 1;
-        }
-        if (modeMap[arr[i]] > modeCount) {
-            mode = arr[i];
-            modeCount = modeMap[arr[i]];
-        }
-    }
-
-    var mean = sum / arr.length;
-
-    if (mean === mode) {
-        return 1;
-    } else {
-        return 0;
-    }
-
+  if (mode == mean)
+    return 1;
+  else
+    return 0;
 }
