f55545d6e19bd4746d0bd050dcbb4b4fcae11892
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 12:48:02 +0530

    Update Medium - 11 - Caesar Cipher.js

--- Medium - 11 - Caesar Cipher.js
+++ Medium - 11 - Caesar Cipher.js
@@ -1,29 +1,29 @@
 function CaesarCipher(str, num) {
-
-    var alphaStrLow = "abcdefghijklmnopqrstuvwxyz";
-    var alphaStrUpp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var currentLetterInd = 0;
-    var newLetterInd = 0;
-    var answer = "";
-    var lowerTest = /[a-z]/;
-    var upperTest = /[A-Z]/;
-
-    for (i = 0; i < str.length; i++) {
-        if (upperTest.test(str[i])) {
-            currentLetterInd = alphaStrUpp.indexOf(str[i]);
-            newLetterInd = (currentLetterInd + num) % 26;
-            answer += alphaStrUpp[newLetterInd];
-        } 
-      else if (lowerTest.test(str[i])) {
-            currentLetterInd = alphaStrLow.indexOf(str[i]);
-            newLetterInd = (currentLetterInd + num) % 26;
-            answer += alphaStrLow[newLetterInd];
-        } 
-      else {
-            answer += str[i];
-        }
+  var res = '';
+  for (var i = 0; i < str.length; i++) {
+    var c = str[i];
+    if (c >= 'a' && c <= 'z') {
+      res += ReturnEncodedChar(c, 97, 122, num);
     }
-
-    return answer;
-
+    else if (c >= 'A' && c <= 'Z') {
+      res += ReturnEncodedChar(c, 65, 90, num);
+    }
+    else
+      res += c;
+  }
+  return res;
 }
+
+function ReturnEncodedChar(char, aCode, zCode, num) {
+   var code = char.charCodeAt(0);
+   var newCode;
+   if (code == zCode)
+     newCode = aCode - 1 + num;
+   else {
+     var newCode = code + num;
+     if (newCode > zCode) {
+       newCode = aCode - 1 + (newCode - zCode);
+     }
+   }
+   return String.fromCharCode(newCode);
+ }
