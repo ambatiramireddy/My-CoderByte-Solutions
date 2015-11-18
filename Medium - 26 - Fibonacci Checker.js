c0cf5ab3c6cbae3bcdc9456af046bdd92040b0de
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 14:41:58 +0530

    Update Medium - 26 - Fibonacci Checker.js

--- Medium - 26 - Fibonacci Checker.js
+++ Medium - 26 - Fibonacci Checker.js
@@ -1,15 +1,13 @@
-function FibonacciChecker(num) { 
-  var fibSeq = [0,1];
-  for (i=0;i<=100000;i++) {
-  fibSeq.push(fibSeq[i]+fibSeq[i+1]);
-    
+function FibonacciChecker(num) {
+  function Recursive(n1, n2) {
+    var n = n1 + n2;
+    if (n == num)
+      return true;
+    else if (n < num)
+      return Recursive(n2, n);
   }
-  if (fibSeq.indexOf(num) != -1) {
-  return "yes";
-  } 
-  else  {
-  return "no";
-  }
-    
-         
+  if (Recursive(0, 1))
+    return 'yes'
+  else
+    return 'no';
 }
