6439e38b1fdafbbafd3cf7a93e627d94c0d0a1be
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 12:17:37 +0530

    Update Easy - 27 - Powers of Two.js

--- Easy - 27 - Powers of Two.js
+++ Easy - 27 - Powers of Two.js
@@ -1,9 +1,10 @@
 function PowersofTwo(num) { 
-  
-while (num > 1)  {
-  if (num%2 !== 0) {
-    return false;}
-num = Math.floor(num/2);
-}
-return true;
+  var p=1,i=0;
+  while(p<=num)
+  {
+     var p=Math.pow(2,i);
+     if(p==num) return 'true';
+     i++;
+  }  
+  return 'false'; 
 }
