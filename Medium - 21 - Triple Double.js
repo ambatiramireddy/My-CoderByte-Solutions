8d6ae21500be9a74c4a8ff3d91c632bd9e0f638b
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 13:06:05 +0530

    Update Medium - 21 - Triple Double.js

--- Medium - 21 - Triple Double.js
+++ Medium - 21 - Triple Double.js
@@ -1,25 +1,12 @@
 function TripleDouble(num1, num2) {
-
-    num1 = num1.toString();
-    num2 = num2.toString();
-    var triples = [];
-    var doubles = [];
-
-    for (var i = 0; i < num1.length; i++) {
-        if ((num1[i] == num1[i + 1]) && (num1[i] == num1[i + 2])) {
-            triples.push(num1[i] + num1[i + 1]);
-        }
+  var str1 = num1.toString();
+  var str2 = num2.toString();
+  for (var i = 0; i <= 9; i++) {
+    if (str1.indexOf(i + '' + i + '' + i) != -1) {
+      if (str2.indexOf(i + '' + i) != -1) {
+        return 1;
+      }
     }
-    for (var j = 0; j < num2.length; j++) {
-        if (num2[j] == num2[j + 1]) {
-            doubles.push(num2[j] + num2[j + 1]);
-        }
-    }
-
-    for (var k = 0; k < triples.length; k++) {
-        if (triples.indexOf(doubles[k]) != -1) {
-            return 1;
-        }
-    }
-    return 0;
+  }
+  return 0;
 }
