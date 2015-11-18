2f8aa0b2c4e01de9ee5c3d66a874d84f9af2259a
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 12:31:28 +0530

    Update Easy - 30 - Off Line Minimum.js

--- Easy - 30 - Off Line Minimum.js
+++ Easy - 30 - Off Line Minimum.js
@@ -1,26 +1,27 @@
-function OffLineMinimum(strArr) {
+function OffLineMinimum(strArr) { 
+  var output = [];
+  recursion();
+  return output.join(',');
 
-    var numTest = /[0-9]/;
-    var numArr = [];
-    var answerArr = [];
-    var lowest = 10;
-    var lowestInd = 0;
-
+  function recursion() {
     for (var i = 0; i < strArr.length; i++) {
-        if (numTest.test(strArr[i])) {
-            numArr.push(strArr[i]);
-        } else {
-            for (var j = 0; j < numArr.length; j++) {
-                if (numArr[j] < lowest) {
-                    lowest = numArr[j];
-                    lowestInd = numArr.indexOf(lowest);
-                }
-            }
-            answerArr.push(lowest);
-            lowest = 10;
-            numArr.splice(lowestInd, 1);
-        }
-    }
-
-    return answerArr.join(",");
+      var v = strArr[i];
+      if (v == 'E') {
+        //to create subset with all integers before 'E'
+        var arr = strArr.slice(0, i);
+        //to sort in descending order and return minimum value
+        var item = arr.sort(function (a, b) { return parseInt(b) - parseInt(a); }).pop();
+        output.push(item);
+        //to remove 'E' element
+        strArr.splice(i, 1);
+        //to remove minimum integer found in the subset from inpur array
+        strArr.splice(strArr.indexOf(item), 1);
+        break;
+      }
+    } //for end
+    if (strArr.indexOf('E') == -1)
+      return;
+    else
+      recursion();
+  }      
 }
