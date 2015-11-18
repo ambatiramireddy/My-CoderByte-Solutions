b35a1668a007421930720904cff6218764719910
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 14:40:25 +0530

    Update Medium - 24 - Three Five Multiples.js

--- Medium - 24 - Three Five Multiples.js
+++ Medium - 24 - Three Five Multiples.js
@@ -1,9 +1,9 @@
 function ThreeFiveMultiples(num) { 
-  var sum = 0;
-  for (var i = 0; i < num; i++) {
-    if ((i % 3 === 0) || (i % 5 === 0)) {
-      sum += i;
-    }
+  var res=0;
+  for(var i=1;i<num;i++)
+  {
+    if(i%3==0 || i%5==0)
+      res+=i;
   } 
-  return sum; 
+  return res;      
 }
