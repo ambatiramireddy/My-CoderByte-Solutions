38e25e3da2c7e197accb0309e8ada4dd83ded780
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 12:40:22 +0530

    Update Medium - 05 - Division.js

--- Medium - 05 - Division.js
+++ Medium - 05 - Division.js
@@ -1,17 +1,7 @@
-function Division(num1, num2) {
-    var largestFactor = 1;
-    var smallerNum;
-    
-    if (num1 > num2) {
-        smallerNum = num2;
-    } else {
-        smallerNum = num1;
-    }
-
-    for (i = 1; i <= smallerNum; i++) {
-        if ((num1 % i === 0) && (num2 % i === 0) && (i > largestFactor)) {
-            largestFactor = i;
-        }
-    }
-    return largestFactor;
+function Division(num1,num2) { 
+  var lowest = num1 > num2 ? num2 : num1;
+  for (var i = lowest; i >= 1; i--) {
+    if (num1 % i == 0 && num2 % i == 0)
+      return i;
+  }
 }
