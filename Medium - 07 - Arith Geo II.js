4685dc10143a2130d61a32c05912d1f623acdb6c
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 12:43:12 +0530

    Update Medium - 07 - Arith Geo II.js

--- Medium - 07 - Arith Geo II.js
+++ Medium - 07 - Arith Geo II.js
@@ -1,31 +1,24 @@
-function ArithGeoII(arr) { 
-  
-  var diffArr = [];
-  var geoArr = [];
-  var geoFlag = true;
-  var arithFlag = true;
-  
+function ArithGeoII(arr) {
+  var diff = arr[1] - arr[0];
+  var arithmetic = true;
   for (var i = 0; i < arr.length - 1; i++) {
-  diffArr.push(arr[i+1]-arr[i]);
-  geoArr.push(arr[i+1]/arr[i]);
+    if (arr[i + 1] - arr[i] != diff)
+      arithmetic = false;
   }
-  
-  for (var j = 0; j < diffArr.length - 1; j++) {
-    if (diffArr[j] !== diffArr[j+1]) {
-      arithFlag = false;
-    }
-    if (geoArr[j] !== geoArr[j+1]) {
-      geoFlag = false;    
+
+  var geometric = true;
+  if (!arithmetic) {
+    diff = arr[1] / arr[0];
+    for (var i = 0; i < arr.length - 1; i++) {
+      if (arr[i + 1] / arr[i] != diff)
+        geometric = false;
     }
   }
 
-  if (arithFlag) {
-    return "Arithmetic";
-  }
-  else if (geoFlag) {
-    return "Geometric";
-  }
-  else {
+  if (arithmetic)
+    return 'Arithmetic';
+  else if (geometric)
+    return 'Geometric';
+  else
     return -1;
-  }
 }
