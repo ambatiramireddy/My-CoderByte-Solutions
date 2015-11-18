5b21ad093561249233be024259a6a46635f1dc4a
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 12:34:55 +0530

    Update Medium - 01 - Prime Time.js

--- Medium - 01 - Prime Time.js
+++ Medium - 01 - Prime Time.js
@@ -1,11 +1,10 @@
-function PrimeTime(num) {
-    if (num === 1) {
-        return false;
-    }
-    for (var i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
-    return true;
+function PrimeTime(num) { 
+  if(num<=1)
+    return 'false';
+  for(var i=2;i<num;i++)
+  {
+    if(num%i==0)
+      return 'false';
+  }
+  return 'true' ;    
 }
