bb5be85135a6c1c18e10b2a0776460ef8855fa52
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 14:49:27 +0530

    Update Medium - 30 - Distinct List.js

--- Medium - 30 - Distinct List.js
+++ Medium - 30 - Distinct List.js
@@ -1,14 +1,10 @@
 function DistinctList(arr) { 
-
-  arr = arr.sort(function(a,b){return a-b});
-  var startLength = arr.length;
-    for (i=arr.length-1;i>0;i--) {
-      if (arr[i] === arr[i-1]) {
-      arr.splice(i,1);
-      }
-    }
-  var endLength = arr.length;
-  
-  return startLength - endLength; 
-         
+  var distinct=[];
+  for(var i=0;i<arr.length;i++)
+  {
+    var n=arr[i];
+    if(distinct.indexOf(n)==-1)
+    	distinct.push(n);      
+  } 
+  return arr.length-distinct.length;       
 }
