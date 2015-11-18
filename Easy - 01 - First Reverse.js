9deb1618577c2e1ddf39f0a4e61f2d1b9ce9240f
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-18 10:51:10 +0530

    Update Easy - 01 - First Reverse.js

--- Easy - 01 - First Reverse.js
+++ Easy - 01 - First Reverse.js
@@ -1,23 +1,6 @@
-// Step By Step
-function FirstReverse(str) {
-    
-    // First, we need to use .split to change the string into an array so it can be further manipulated.
-    // Passing "" (an empty string) into the method splits the string at every character.
-    str = str.split("");
-
-    // Next, we use the reverse method to reverse the order of the elements in our newly formed array.
-    // Note that only arrays have the .reverse method, which is why we had to use .split.
-    str = str.reverse();
-
-    // After that, we use the .join method to reassemble our array back into a string. 
-    // Passing an empty string here means that nothing gets inserted between each array element.
-    str = str.join("");
-
-    // Finally, we return the modified string
-    return str;
-}
-
-// Using chaining
-function FirstReverse(str) {
-    return str.split("").reverse().join("");
+function FirstReverse(str) { 
+  var res='';
+  for(var i=str.length-1;i>=0;i--)
+    res+=str[i];
+  return res;        
 }
