e3f19748a72647ae453046f0cd4c7238e152635c
ambatiramireddy <ambati.ramireddy@gmail.com>
2015-11-19 14:32:54 +0530

    Update Easy - 04 - Letter Changes.js

--- Easy - 04 - Letter Changes.js
+++ Easy - 04 - Letter Changes.js
@@ -1,34 +1,18 @@
-function CapitalizeVowels(str)
-{
-  var res='';
-  for(var i=0;i<str.length;i++)
-  {
-   var c=str[i];
-   if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
-     res+=str[i].toUpperCase();
-    else
-      res+=str[i];
-  }
-  return res;
-}
-
 function LetterChanges(str) { 
-  var res='';
-  for(i=0;i<str.length;i++)
-  {
-    var c=str.charCodeAt(i);
-    if(c>=97 && c<122)
-    {
-      res+=String.fromCharCode(c+1);
-    }
-    else if(c==122)
-    {
-      res+=String.fromCharCode(97);
-    }
+  var vowels = 'aeiou', result = '', changedChar;
+  for (i = 0; i < str.length; i++) {
+    var c = str.charCodeAt(i);
+    if (c >= 97 && c < 122)
+      changedChar = String.fromCharCode(c + 1);
+    else if (c == 122)
+      changedChar = String.fromCharCode(97);
     else
-    {
-      res+=str[i];
-    }
+      changedChar = str[i];
+
+    if (vowels.indexOf(changedChar) != -1)
+      changedChar = changedChar.toUpperCase();
+
+    result += changedChar;
   }
-  return CapitalizeVowels(res);         
+  return result;
 }
